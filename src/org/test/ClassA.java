4e11aa34f79ecbda78bb26c54a7118d470889ce0
NivasNanthini <nivas.srinins1995@gmail.com>
2025-01-13 10:00:26 +0530

    Sri

---
+++ src/org/test/ClassA.java
@@ -0,0 +1,15 @@
+package org.test;
+
+public class ClassA {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Test 1");
+		System.out.println("Test 2");
+		System.out.println("Test 3");
+		
+		
+		
+	}
+
+}
